3dbc1011c90da569cefd6b8e855119b897817cce
Ashish Nayak <ashish04021989@gmail.com>
2025-09-10 18:19:47 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -10,21 +10,40 @@ import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App font-sans text-gray-800">
       <Navbar />
-      <div className="flex flex-col md:flex-row">
-        <div className="md:w-1/4">
+
+      <section id="hero">
+        <Hero />
+      </section>
+
+      <section id="about" className="mt-20">
+        <About />
+      </section>
+
+      <div className="flex flex-col md:flex-row max-w-7xl mx-auto mt-16 px-4 md:px-8 gap-8">
+        <aside className="md:w-1/4">
           <Skills />
-        </div>
-        <div className="md:w-3/4">
-          <Hero />
-          <About />
-          <Experience />
-          <Education />
-          <Certifications />
-          <Contact />
-        </div>
+        </aside>
+
+        <main className="md:w-3/4 space-y-12">
+          <section id="experience">
+            <Experience />
+          </section>
+
+          <section id="education">
+            <Education />
+          </section>
+
+          <section id="certifications">
+            <Certifications />
+          </section>
+        </main>
       </div>
+
+      <section id="contact" className="mt-16 px-4 md:px-8">
+        <Contact />
+      </section>
     </div>
   );
 }
